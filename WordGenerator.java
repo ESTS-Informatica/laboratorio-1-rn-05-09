c50395fcd9dd92f38d418cd3d53129e8d92f3a88
Rafael Quintas <202300310@estudantes.ips.pt>
2024-03-13 10:17:55 +0000

    Nível 3

---
+++ WordGenerator.java
@@ -0,0 +1,34 @@
+/**
+ * Escreva uma descrição da classe WordGenerator aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+
+import java.util.ArrayList;
+import java.util.Random;
+public class WordGenerator {
+    private ArrayList<String> words;
+    private Random random;
+    
+    public WordGenerator() {
+        words = new ArrayList<String>();
+        random = new Random();
+    }
+    
+    private void fillArrayList() {
+        String[] wordsToAdd = {"boolean", "break", "byte", "case", "char", "class", "continue", "do", "double", "else", "enum", "for", "if", "import", "int"};
+        for (String add : wordsToAdd) {
+            words.add(add);
+        }
+    }
+    
+    public void generateWord() {
+        int index = random.nextInt(words.size() + 1);
+        System.out.println("A palavra gerada foi: " + words.get(index));
+    }
+    
+    public void addWord(String add) {
+        words.add(add);
+    }
+}
